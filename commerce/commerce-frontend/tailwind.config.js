12979dcd71be402674c0302742fd9d0dd40f6351
Samuel Joseph <samuel.joseph2005@gmail.com>
2025-06-12 21:32:14 +0530

    latest

---
+++ commerce/commerce-frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+        secondary: '#3B82F6',
+      },
+    },
+  },
+  plugins: [],
+};
